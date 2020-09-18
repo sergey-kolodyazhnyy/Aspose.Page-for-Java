132664fd3e347527557951534be29f76534b7710
sergey-kolodyazhnyy <45591614+sergey-kolodyazhnyy@users.noreply.github.com>
2020-09-18 16:54:45 +0700

    Updated code to the current version (20.8).

--- Examples/src/main/java/com/aspose/page/shapes/AddEllipse.java
+++ Examples/src/main/java/com/aspose/page/shapes/AddEllipse.java
@@ -28,17 +28,13 @@ public class AddEllipse {
         XpsDocument doc = new XpsDocument();
         // Radial gradient stroked ellipse in the lower left
         List<XpsGradientStop> stops = new LinkedList<XpsGradientStop>();
-
-        // Radial gradient stroked ellipse in the lower left
         stops.add(doc.createGradientStop(doc.createColor(0, 0, 255), 0f));
         stops.add(doc.createGradientStop(doc.createColor(255, 0, 0), .25f));
         stops.add(doc.createGradientStop(doc.createColor(0, 255, 0), .5f));
         stops.add(doc.createGradientStop(doc.createColor(255, 255, 0), .75f));
         stops.add(doc.createGradientStop(doc.createColor(255, 0, 0), 1f));
+
         XpsPath path = doc.addPath(doc.createPathGeometry("M 20,250 A 100,50 0 1 1 220,250 100,50 0 1 1 20,250"));
-        // New canvas
-        XpsCanvas canvas = doc.addCanvas();
-        path = canvas.addPath(path);
         path.setStroke(doc.createRadialGradientBrush(new Point2D.Float(575f, 125f), new Point2D.Float(575f, 100f), 75f, 50f));
         ((XpsGradientBrush)path.getStroke()).setSpreadMethod(XpsSpreadMethod.Reflect);
         ((XpsGradientBrush)path.getStroke()).getGradientStops().addAll(stops);
