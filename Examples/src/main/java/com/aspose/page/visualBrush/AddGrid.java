ecd7eacbdd15bbc78faa12bd991d0cc245220de3
sergey-kolodyazhnyy <45591614+sergey-kolodyazhnyy@users.noreply.github.com>
2020-09-18 17:23:42 +0700

    Updated code to the current version (20.8).

--- Examples/src/main/java/com/aspose/page/visualBrush/AddGrid.java
+++ Examples/src/main/java/com/aspose/page/visualBrush/AddGrid.java
@@ -26,20 +26,21 @@ public class AddGrid {
         // Create new XPS Document
         XpsDocument doc = new XpsDocument();
 
-        // Canvas for magenta grid VisualBrush
-        XpsCanvas visualCanvas = doc.createCanvas();
-        XpsPath visualPath = visualCanvas.addPath(
-                        doc.createPathGeometry("M 0,4 L 4,4 4,0 6,0 6,4 10,4 10,6 6,6 6,10 4,10 4,6 0,6 Z"));
-        visualPath.setFill(doc.createSolidColorBrush(doc.createColor(1f, .61f, 0.1f, 0.61f)));
-
         // Geometry for magenta grid VisualBrush
         XpsPathGeometry pathGeometry = doc.createPathGeometry();
         pathGeometry.addSegment(doc.createPolyLineSegment(
-                        new Point2D.Float[] { new Point2D.Float(240f, 5f), new Point2D.Float(240f, 310f), new Point2D.Float(0f, 310f) }));
-        pathGeometry.getFigure(0).setStartPoint(new Point2D.Float(0f, 5f));
+            new Point.Float[] { new Point.Float(240f, 5f), new Point.Float(240f, 310f), new Point.Float(0f, 310f) }));
+        pathGeometry.get(0).setStartPoint(new Point.Float(0f, 5f));
+        
+        // Canvas for magenta grid VisualBrush
+        XpsCanvas visualCanvas = doc.createCanvas();
 
-        // Path for magenta grid
+        XpsPath visualPath = visualCanvas.addPath(
+            doc.createPathGeometry("M 0,4 L 4,4 4,0 6,0 6,4 10,4 10,6 6,6 6,10 4,10 4,6 0,6 Z"));        
+        visualPath.setFill(doc.createSolidColorBrush(doc.createColor(1f, .61f, 0.1f, 0.61f)));
+
         XpsPath gridPath = doc.createPath(pathGeometry);
+        //Create Visual Brush, it is specified by some XPS fragment (vector graphics and glyphs)
         gridPath.setFill(doc.createVisualBrush(visualCanvas,
                         new Rectangle2D.Float(0f, 0f, 10f, 10f), new Rectangle2D.Float(0f, 0f, 10f, 10f)));
         ((XpsVisualBrush)gridPath.getFill()).setTileMode(XpsTileMode.Tile);
@@ -48,9 +49,10 @@ public class AddGrid {
         XpsCanvas canvas = doc.addCanvas();
         canvas.setRenderTransform(doc.createMatrix(1f, 0f, 0f, 1f, 268f, 70f));
         // Add grid
-        canvas.addPath(gridPath);
+        canvas.addPath(pathGeometry);
         // Red transparent rectangle in the middle top
-        XpsPath path = canvas.addPath(doc.createPathGeometry("M 10,10 L 228,10 228,100 10,100"));
+        XpsPath path = canvas.addPath(doc.createPathGeometry("M 30,20 l 258.24,0 0,56.64 -258.24,0 Z"));
+        path = canvas.addPath(doc.createPathGeometry("M 10,10 L 228,10 228,100 10,100"));
         path.setFill(doc.createSolidColorBrush(doc.createColor(1.0f, 0.0f, 0.0f)));
         path.setOpacity(0.7f);
         // Save resultant XPS document
